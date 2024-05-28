e55671db004c3f4f154f0180ad427bbe720fa5de
Vadim J. Bychok <vadim@bychok.net>
2024-05-28 11:47:17 -0700

    Adding eggs at set interval

--- script.js
+++ script.js
@@ -164,16 +164,20 @@ window.addEventListener("load", function () {
   class Egg {
     constructor(game) {
       this.game = game;
-      this.collisionX = Math.random() * this.game.width;
-      this.collisionY = Math.random() * this.game.height;
       this.collisionRadius = 40;
+      this.margin = this.collisionRadius * 2;
+      this.collisionX =
+        this.margin + Math.random() * (this.game.width - this.margin * 2);
+      this.collisionY =
+        this.game.topMargin +
+        Math.random() * (this.game.height - this.game.topMargin);
       this.image = document.getElementById("egg");
       this.spriteWidth = 110;
       this.spriteHeight = 135;
       this.width = this.spriteWidth;
       this.height = this.spriteHeight;
-      this.spriteX = this.collisionX + this.width * 0.5;
-      this.spriteY = this.collisionY + this.height * 0.5;
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5 - 30;
     }
     draw(context) {
       context.drawImage(this.image, this.spriteX, this.spriteY);
@@ -206,6 +210,8 @@ window.addEventListener("load", function () {
       this.fps = 70;
       this.timer = 0;
       this.interval = 1000 / this.fps;
+      this.eggTimer = 0;
+      this.eggInterval = 500;
       this.numberOfObstacles = 10;
       this.maxEggs = 10;
       this.obstacles = [];
@@ -243,11 +249,22 @@ window.addEventListener("load", function () {
         this.obstacles.forEach((obstacle) => {
           obstacle.draw(context);
         });
+        this.eggs.forEach((egg) => {
+          egg.draw(context);
+        });
         this.player.draw(context);
         this.player.update();
         this.timer = 0;
       }
       this.timer += deltaTime;
+
+      // add eggs periodically
+      if (this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs) {
+        this.addEgg();
+        this.eggTimer = 0;
+      } else {
+        this.eggTimer += deltaTime;
+      }
     }
     checkCollision(a, b) {
       const dx = a.collisionX - b.collisionX;
@@ -256,7 +273,9 @@ window.addEventListener("load", function () {
       const sumOfRadii = a.collisionRadius + b.collisionRadius;
       return [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
     }
-    addEgg() {}
+    addEgg() {
+      this.eggs.push(new Egg(this));
+    }
     init() {
       let attempts = 0;
       while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
